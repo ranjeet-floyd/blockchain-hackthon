0e792159d581e95ebe2f4deee40bc69e3607d00b
Kumar, Ranjeet <ranjeet.kumar@rakuten.com>
2018-02-23 04:16:55 +0530

    no charge for reg and sold

--- contracts/SellerPlatform.sol
+++ contracts/SellerPlatform.sol
@@ -1,62 +1,75 @@
 pragma solidity ^0.4.6;
 
 contract SellerPlatform {
-    struct ProductInfo {
-        // register, sold, deliver
-        string Status; 
-
-    }
+    
     //Ref : https://coursetro.com/posts/code/102/Solidity-Mappings-&-Structs-Tutorial
     struct Product {
         string Name;
         uint Id;
-        ProductInfo ProductInfos;
-
+        string Status;
+        uint Quantity;
     }
 
     
     
     // Maintain product block history 
-    mapping(uint => Product) Products;
+    mapping(uint => Product) ProductReg;
+    uint8 ProductRegCount=0;
 
-    // uint[] public ProductIds;
+
+    // sold product
+     mapping(uint => Product) ProductSold;
+     uint8 ProductSoldCount=0;
+
+    // delibver product
+     mapping(uint => Product) ProductDelivered;
+     uint8 ProductDeliverCount=0;
 
 
-    function CreateProduct(string Name) {
+    function RegisterProduct(string Name) constant returns(string, uint, string) {
         Product memory newProduct;
         newProduct.Name = Name;
-        newProduct.Id = 1233;
-        // ProductIds.push(newProduct.Id);
-        // product status
-        ProductInfo memory newProductInfo ;
-        newProductInfo.Status = "REGISTER";
-        newProduct.ProductInfos = newProductInfo;
-        Products[newProduct.Id] = newProduct;
+        newProduct.Quantity = 1;
+        newProduct.Status = "REGISTERED";
+        newProduct.Id = ProductRegCount;//now();
+        ProductReg[ProductRegCount] = newProduct;    
+        ProductRegCount++;
+        return (newProduct.Name,newProduct.Id,newProduct.Status);
     }
 
     // registration of product
-    function sellProduct(uint productId) {
-        var product = Products[productId];
-        // product status
-        ProductInfo memory newProductInfo ;
-        newProductInfo.Status = "SOLD";
-        product.ProductInfos = newProductInfo;
-        Products[product.Id] = product;
+    function sellProduct(uint ProductId) constant returns(string) {
+        var newProduct = ProductReg[ProductId];
+        newProduct.Status = "SOLD";
+        ProductSold[ProductSoldCount] = newProduct;
+        ProductSoldCount++;
+        return newProduct.Name;
 
     }
 
-    function DeliverProduct(uint productId) {
-         var product = Products[productId];
-        // product status
-        ProductInfo memory  newProductInfo ;
-        newProductInfo.Status = "DELIVER";
-        product.ProductInfos = newProductInfo;
-        Products[product.Id] = product;
+    function DeliverProduct(uint ProductId) {
+        var newProduct = ProductReg[ProductId];
+        newProduct.Status = "DELIVERED";
+        ProductReg[ProductDeliverCount] = newProduct;
+        ProductDeliverCount++;
         
     }
 
+    function GetRegProductCount() constant returns(uint) {
+        return ProductRegCount;
+        
+    }
 
-    function GetProduct(uint productId) returns (string) {
-        return (Products[productId].Name);
+    function GetSoldProductCount() constant returns(uint) {
+        return ProductSoldCount;
+        
+    }
+
+    function GetDeliveredProductCount()  constant returns(uint) {
+        return ProductDeliverCount;
+        
+    }
+    function GetProduct(uint ProductId) constant returns (string, uint) {
+        return (ProductReg[ProductId].Name, ProductReg[ProductId].Id);
     }
 }

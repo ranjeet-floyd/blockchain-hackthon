2639c67acd8ac401ae2da42d2c809a8deceb04d7
abhishek.kumar <abhishek.kumar@rakuten.com>
2018-02-22 23:15:26 +0530

    adding sller p reg

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
   plugins: [
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" }
+      { from: './app/index.html', to: "index.html" },
+      { from: './app/seller-register.html', to: "seller-register.html" }
     ])
   ],
   module: {
